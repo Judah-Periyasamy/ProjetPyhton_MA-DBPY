ad46c85a044eefd78cf3bdb6f115ea1fb14dd48f
judah.periyasamy <judah.periyasamy@eduvaud.ch>
2023-09-01 08:54:51 +0200

    Function choose menu

--- main.py
+++ main.py
@@ -5,14 +5,33 @@ Author : Judah Periyasamy
 Version : 1.0
 """
 
-print("""
-1. Afficher l’ordre en classe
-2. Générer le planning « Ordre en classe »
-3. Valider l’ordre en classe de la semaine
-4. Supprimer un élève de la liste
-5. Ajouter un élève de la liste
-6. Générer le document « Ordre en classe »
-7. Sortir du menu """)
-
 def choose_menu():
-    user = int(input("Veuillez choisir u n menu : "))
+    print("""
+    1. Afficher l’ordre en classe
+    2. Générer le planning « Ordre en classe »
+    3. Valider l’ordre en classe de la semaine
+    4. Supprimer un élève de la liste
+    5. Ajouter un élève de la liste
+    6. Générer le document « Ordre en classe »
+    7. Sortir du menu """)
+    print("")
+    while True:
+        try:
+            user_menu = int(input("Veuillez choisir un menu : "))
+            while user_menu < 1 or user_menu > 7:
+                print("Menu inéxistant")
+                print("")
+                user_menu = int(input("Veuillez choisir un menu : "))
+            while user_menu == 1 or user_menu == 2 or user_menu == 3 or user_menu == 4 or user_menu == 5 or user_menu == 6:
+                print("Menu pas encore fonctionnel")
+                print("")
+                user_menu = int(input("Veuillez choisir un menu : "))
+            if user_menu == 7:
+                print("Vous allez sortir du menu")
+                quit()
+        except ValueError:
+            print("Il faut choisir un menu entre 1 et 7")
+            print("")
+
+choose_menu()
+
