91baf1b36bec9b34c66635fa6d6459d8eab9080b
judah.periyasamy <judah.periyasamy@eduvaud.ch>
2023-09-01 08:21:19 +0200

    Display in string of the menu and start of the menu function

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ Title: Projet Python Ordre de classe
 Author : Judah Periyasamy
 Version : 1.0
 """
+
+print("""
+1. Afficher l’ordre en classe
+2. Générer le planning « Ordre en classe »
+3. Valider l’ordre en classe de la semaine
+4. Supprimer un élève de la liste
+5. Ajouter un élève de la liste
+6. Générer le document « Ordre en classe »
+7. Sortir du menu """)
+
+def choose_menu():
+    user = int(input("Veuillez choisir u n menu : "))
