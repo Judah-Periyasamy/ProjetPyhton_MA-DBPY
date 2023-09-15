60cb6b128390be2cfacb7f4af6d667a90dbc51b5
judah.periyasamy <judah.periyasamy@eduvaud.ch>
2023-09-15 11:20:21 +0200

    Menu add student and delete student finished

---
+++ database.py
@@ -0,0 +1,60 @@
+"""
+MA-DBPY : Python + DB
+Title: Projet Python Ordre de classe
+Author : Judah Periyasamy
+Version : 1.0
+"""
+import mysql.connector
+
+def open_dbconnection():
+    global db_connection
+    db_connection = mysql.connector.connect(host='127.0.0.1', port='3306',
+                                   user='judah', password='Jukuporter392.', database='classroom_cleaning',
+                                   buffered=True, autocommit=True)
+    return db_connection
+def close_dbconnection():
+    db_connection.close()
+
+def get_class_info(class_name):
+    cursor = db_connection.cursor()
+    query = "SELECT id, name, room from classes where name = %s"
+    cursor.execute(query, (class_name,))
+    row = cursor.fetchone()
+    cursor.close()
+    return row
+
+def delete_student(user_input, user_input2, user_input3):
+    query = "DELETE FROM students WHERE lastname = %s AND firstname = %s and class_id = (select id from classes where name = %s)"
+    cursor = db_connection.cursor()
+    cursor.execute(query, (user_input, user_input2, user_input3))
+    affected_rows = cursor.rowcount
+    cursor.close()
+    if affected_rows == 1:
+        return True
+    else:
+        return False
+
+def add_student(lastname, firstname, student_email, classname):
+    class_info = get_class_info(classname)
+    if class_info is None:
+        raise Exception('Erreur dans le nom de la classe, vérifiez-le.')
+    else:
+        cursor = db_connection.cursor()
+        # first column is the id of the class
+        class_id = class_info[0]
+        query = "insert into students (lastname, firstname, email, class_id) values (%s, %s, %s, %s)"
+        cursor.execute(query, (lastname, firstname, student_email, class_id))
+        affected_rows = cursor.rowcount
+        cursor.close()
+        if affected_rows == 1:
+            return True
+        else:
+            return False
+
+def get_student_info(user_input,user_input2):
+    query = "SELECT firstname, lastname FROM classes WHERE lastname = %s and firstname = %s"
+    cursor = db_connection.cursor()
+    cursor.execute(query, (user_input,user_input2))
+    row = cursor.fetchone()
+    cursor.close()
+    return row
--- main.py
+++ main.py
@@ -5,38 +5,92 @@ Author : Judah Periyasamy
 Version : 1.0
 """
 from import_student import *
-def delete_student():
-    pass
+from database import *
+
+def get_info_student():
+    """
+    ask the user information about the student
+    :return: a dictionnary with lastname, firstname and classname the user wrote
+    """
+    last_name = input("Veuillez entre le nom de l'élève : ")
+    first_name = input("Veuillez entre le prénom de l'élève : ")
+    class_name = get_classname_student()
+    return {'lastname': last_name, 'firstname': first_name, 'classname': class_name}
+
+
+def get_classname_student():
+    """
+    ask user the classname to generate the planning
+    :return: class_name
+    """
+    class_name = input("Veuillez entre la classe de l'élève : ")
+    return class_name
+
+
+def menu_info():
+    print("Menu")
+    print("1. Afficher l'ordre en classe")
+    print("2. Générer le planning \"Ordre en classe\"")
+    print("3. Valider l'ordre en classe de la semaine")
+    print("4. Supprimer un élève de la liste")
+    print("5. Ajouter un élève de la liste")
+    print("6. Générer le document \"Ordre en classe\"")
+    print("7. Sortir du menu")
+    print("")
 
 def choose_menu():
-    list_menu = print("""
-    1. Afficher l’ordre en classe
-    2. Générer le planning « Ordre en classe »
-    3. Valider l’ordre en classe de la semaine
-    4. Supprimer un élève de la liste
-    5. Ajouter un élève de la liste
-    6. Générer le document « Ordre en classe »
-    7. Sortir du menu """)
-    print("")
+    menu_info()
     while True:
+        open_dbconnection()
         try:
             user_menu = int(input("Veuillez choisir un menu : "))
             while user_menu < 1 or user_menu > 7:
                 print("Menu inéxistant")
                 print("")
+                menu_info()
                 user_menu = int(input("Veuillez choisir un menu : "))
-            while user_menu == 1 or user_menu == 2 or user_menu == 3 or user_menu == 4 or user_menu == 5 or user_menu == 6:
+            while user_menu == 1 or user_menu == 2 or user_menu == 3 or user_menu == 6:
                 print("Menu pas encore fonctionnel")
-                print("TEST")
                 print("")
+                menu_info()
                 user_menu = int(input("Veuillez choisir un menu : "))
-            if user_menu == 7:
+            if user_menu == 4:
+                info_student = get_info_student()
+                last_name = info_student['lastname']
+                first_name = info_student['firstname']
+                class_name = info_student['classname']
+                res = delete_student(last_name, first_name, class_name)
+                if last_name == None or first_name == None or class_name == None:
+                    print("Il manque une information !")
+                if res:
+                    print("Suppression réussie.\n")
+                else:
+                    print("Echec de la suppression. Assurez-vous que le nom, prénom et la classe de l'élève sont corrects.\n")
+            elif user_menu == 5:
+                info_student = get_info_student()
+                last_name = info_student['lastname']
+                first_name = info_student['firstname']
+                email = input("Veuillez entrer l'email de l'élève : ")
+                class_name = info_student['classname']
+                try:
+                    res = add_student(last_name, first_name, email, class_name)
+                    if res:
+                        print("Ajout réussi.\n")
+                    else:
+                        print(
+                            "Echec de l'ajout. Assurez-vous que le nom, prénom et la classe de l'élève sont corrects.\n")
+                except Exception as exc:
+                    print(exc)
+                    print("")
+            elif user_menu == 7:
                 print("Vous allez sortir du menu")
                 quit()
         except ValueError:
             print("Il faut choisir un menu entre 1 et 7")
-            print("TEST")
+            choose_menu()
             print("")
+        menu_info()
+        close_dbconnection()
+
 
 choose_menu()
-
